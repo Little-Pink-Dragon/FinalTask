2482fa437944114f1d06da1e12085503ae5eb7d1
Dmtry <nik.naumo2012@yandex.ru>
2023-03-27 01:46:38 +0300

    The middle of the road

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
-﻿Console.Write("Сколько слов и цифр Вы хотите ввести? ");
-int size = Convert.ToInt32(Console.ReadLine());
+﻿Random rnd = new Random();
+int R = rnd.Next(1, 7);
+Console.WriteLine($"Введите {R} слов(а) или цифр:");
+int size = R;
 
-string[] arrayStrings = new string[size];
+string[] arrayInitial = new string[size];
 for (int i = 0; i < size; i++)
 {
  Console.WriteLine($"Введите {i+1}-ое слово или цифру: ");
  string word = Convert.ToString(Console.ReadLine());
- arrayStrings[i] = word;
+ arrayInitial[i] = word;
+}
+string[] arrayFinite = new string[size];
+int len = 3;
+int pos = 0;
+
+for (int j = 0; j < R; j++)
+{
+ if (arrayInitial[j].Length <= len)
+    {
+ arrayFinite[pos] = arrayInitial[j];
+ pos++;
+    }
 }
 
